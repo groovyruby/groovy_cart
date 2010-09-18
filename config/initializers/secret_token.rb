91a23a26fee88231c6ced6335ecdcbf7e019f2bb
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-09-18 09:09:14 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroovyCart::Application.config.secret_token = '9d3594034c052b9a77b00211e14c1ca05c5c743e5cb9aeea9d630174a367d193f4522522b7a5d6ecbc244832fafe6f6f5107e7e554b465449d36db5025c7d8ca'
