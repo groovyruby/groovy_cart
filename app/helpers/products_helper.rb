8c5110197d911a5d0f7c71a3389064febbf2d4e2
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-09-23 17:19:09 +0200

    some fixes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,9 +2,9 @@ module ProductsHelper
   def show_full_price(product)
     if product
       if product.price_promo
-        "<span class=\"old_price\">#{number_to_currency(product.price)}</span><span class=\"price_promo\">#{number_to_currency(product.price_promo)}</span>"
+        raw "<span class=\"old_price\">#{number_to_currency(product.price)}</span><span class=\"price_promo\">#{number_to_currency(product.price_promo)}</span>"
       else
-        "<span class=\"price\">#{number_to_currency(product.price)}</span>"
+        raw "<span class=\"price\">#{number_to_currency(product.price)}</span>"
       end
     else
       ''
@@ -14,9 +14,9 @@ module ProductsHelper
   def show_price(product)
     if product
       if product.price_promo
-        "<span class=\"price\">#{number_to_currency(product.price_promo)}</span>"
+        raw "<span class=\"price\">#{number_to_currency(product.price_promo)}</span>"
       else
-        "<span class=\"price\">#{number_to_currency(product.price)}</span>"
+        raw "<span class=\"price\">#{number_to_currency(product.price)}</span>"
       end
     else
       ''
