f1e70f680c353fdd5cbbfe91794eb1a13a3e1378
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-11-05 17:09:12 +0100

    banner changes it size also while creating new record.

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -4,13 +4,24 @@ class Banner < ActiveRecord::Base
 
   scope :active, where('is_active=?', true)
 
-  has_attached_file :banner, :styles=>{:thumb=>"50x50>", :target=>Proc.new{|i| BannerType.find(i.banner_type_id).get_size } }
+  has_attached_file :banner, :styles=>{:thumb=>"50x50>", :target=>Proc.new{ |i| BannerType.find(i.banner_type_id).get_size } }
 
-  #validates :name, :presence=>true
-  #validates :banner_type_id, :presence=>true
-  #validates :banner_placement_id, :presence=>true
-  #validates :target_url, :presence=>true
+  validates :name, :presence=>true
+  validates :banner_type_id, :presence=>true
+  validates :banner_placement_id, :presence=>true
+  validates :target_url, :presence=>true
 
   attr_accessible :name, :banner_type_id, :banner_placement_id, :banner, :target_url, :is_active
+
+  alias_method :attributes_without_attachment=, :attributes=
+
+  def attributes=(new_attributes, guard_protected_attributes = true)
+    p = new_attributes["banner"] || new_attributes[:banner]
+    @content_type = p.content_type if p
+    send(:attributes_without_attachment=, new_attributes.reject{|key, val| key.to_s == "banner"}, guard_protected_attributes)
+    self.banner = p if p
+  end
+  
+  before_validation(:on => :update){ |b| b.banner = b.banner.to_file(:original) if b.changes['banner_type_id'] }
 
 end
