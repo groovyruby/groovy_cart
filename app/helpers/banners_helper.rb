0286306be30c59223679d1acd822bf875edc6a3d
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-11-05 16:41:15 +0100

    small fix in banners_helper

--- app/helpers/banners_helper.rb
+++ app/helpers/banners_helper.rb
@@ -2,7 +2,9 @@ module BannersHelper
   def load_banner(placement_identifier)
     bp = BannerPlacement.find_by_identifier(placement_identifier)
     banners = bp.banners.active.all
-    banner = banners.sort_by{ rand }.first
-    content_tag(:div, link_to(image_tag(banner.banner.url(:target)), banner.target_url), :id=>"banner_#{banner.id}", :class=>"banner_wrapper")
+    unless banners.empty?
+      banner = banners.sort_by{ rand }.first
+      content_tag(:div, link_to(image_tag(banner.banner.url(:target)), banner.target_url), :id=>"banner_#{banner.id}", :class=>"banner_wrapper")
+    end
   end
 end
