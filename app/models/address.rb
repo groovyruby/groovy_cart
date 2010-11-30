ae3821b9e3febf26c2d22169ec6863d96527c586
Maciej Litwiniuk <maciej@galdomedia.pl>
2010-11-30 16:35:06 +0100

    address cloning

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,29 @@
 class Address < ActiveRecord::Base
   belongs_to :order
 
-  validates :first_name, :last_name, :street, :city, :zip_code, :country, :presence => true
+  validates :first_name, :last_name, :street, :city, :zip_code, :country, :presence => true, :if=>proc{|o| not o.is_shipping? }
   validates :email, :phone, :presence=>true, :if=>proc{|o| not o.is_shipping? }
 
   
   attr_accessible :first_name, :last_name, :street, :city, :zip_code, :country, :email, :phone, :is_shipping
 
+  after_create :clone_address_data
+
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  private
+    def clone_address_data
+      if self.is_shipping?
+        billing = self.order.billing_address
+        self.first_name = billing.first_name if self.first_name.blank?
+        self.last_name = billing.last_name if self.last_name.blank?
+        self.street = billing.street if self.first_name.blank?
+        self.city = billing.city if self.city.blank?
+        self.zip_code = billing.zip_code if self.zip_code.blank?
+        self.country = billing.country if self.country.blank?
+        self.save
+      end
+    end
 end
