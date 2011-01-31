afd211594339b403e16ac4e0dab552abd1ea9723
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-01-31 14:12:22 +0100

    typo

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -1,7 +1,7 @@
 class BannersController < ApplicationController
 
   def show
-    b = banner.find(params[:id])
+    b = Banner.find(params[:id])
     b.clicks_count += 1
     b.save!
     redirect_to b.target_url
